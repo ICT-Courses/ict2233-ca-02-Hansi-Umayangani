8e84990b3cbadf87588a6c7407bf76a66de22c70
Hansi-Umayangani <ar112502@fhss.sjp.ac.lk>
2025-09-21 21:28:54 +0530

    Integrate react-router-dom to enable multi-page navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 import "./index.css"
 
 function App() {
 
   return (
-      <div className="font-sans">
-        <Navbar />
-        <Home />
-      </div>
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
   );
 }
 
