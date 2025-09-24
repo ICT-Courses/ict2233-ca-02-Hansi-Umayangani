e0a7300fa78294d95f7dff1133819e6165b0cc14
Hansi-Umayangani <ar112502@fhss.sjp.ac.lk>
2025-09-24 17:53:41 +0530

    Import GithubProjects component and render GithubProjects section

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,12 +2,14 @@
 
 import { ProjectsHero } from "../components/ProjectsHero";
 import { FeaturedProjects } from "../components/FeaturedProjects";
+import { GitHubProjects } from "../components/GitHubProjects";
 
 export default function Projects() {
   return (
     <div className="flex flex-col">
       <ProjectsHero />
       <FeaturedProjects />
+      <GitHubProjects />
     </div>
   );
 }
