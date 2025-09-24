35f07da1b71a7388bc4135cc73745fa90be8e126
Hansi-Umayangani <ar112502@fhss.sjp.ac.lk>
2025-09-24 21:27:57 +0530

    Render ContactForm component inside grid

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { ContactHero } from "../components/ContactHero"
+import { ContactForm } from "../components/ContactForm"
 
 export default function ContactPage() {
   return (
@@ -8,7 +9,7 @@ export default function ContactPage() {
       <ContactHero />
       <div className="container mx-auto px-4 py-4 md:py-2">
         <div className="grid lg:grid-cols-2 gap-16 max-w-6xl mx-auto">
-
+          <ContactForm />
         </div>
       </div>
     </div>
