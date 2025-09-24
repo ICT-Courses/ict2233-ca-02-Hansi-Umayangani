953c656f689bc024486251167f819ec4ec80d25d
Hansi-Umayangani <ar112502@fhss.sjp.ac.lk>
2025-09-24 12:31:54 +0530

    Import Calender icon from lucid-react

--- src/components/EducationSection.jsx
+++ src/components/EducationSection.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { motion } from "framer-motion"
-import { GraduationCap, BookOpen } from "lucide-react"
+import { GraduationCap, BookOpen, Calendar  } from "lucide-react"
 
 const educationData = [
   {
