7871ee7467a4aa2e79ebb0499995cf456754540a
Hansi-Umayangani <ar112502@fhss.sjp.ac.lk>
2025-09-24 17:27:50 +0530

    Create FeaturedProjects component with section layout

---
+++ src/components/FeaturedProjects.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+export function FeaturedProjects() {
+  return (
+    <section className="py-20">
+      <div className="container mx-auto px-4">
+        {/* Content */}
+      </div>
+    </section>
+  );
+}
