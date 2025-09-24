f8f93068603b5e921a97ff4ea5c2a8506145cdb0
Hansi-Umayangani <ar112502@fhss.sjp.ac.lk>
2025-09-24 17:42:23 +0530

    Import FeaturedProjects component and render FeaturedProjects section

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,13 @@
 "use client"
 
 import { ProjectsHero } from "../components/ProjectsHero";
+import { FeaturedProjects } from "../components/FeaturedProjects";
 
 export default function Projects() {
   return (
     <div className="flex flex-col">
       <ProjectsHero />
+      <FeaturedProjects />
     </div>
   );
 }
