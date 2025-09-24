f73fd86b4209cfbd4961e70bbc8cd6fb7e8e2ef2
Hansi-Umayangani <ar112502@fhss.sjp.ac.lk>
2025-09-24 21:25:28 +0530

    Include ContactHero section in ContactPage

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,11 @@
 "use client"
 
+import { ContactHero } from "../components/ContactHero"
+
 export default function ContactPage() {
   return (
     <div className="flex flex-col bg-background text-foreground min-h-screen">
-      
+      <ContactHero />
     </div>
   )
 }
