8b967b741191098b566c12d13c9ac0e601f0e2fb
Hansi-Umayangani <ar112502@fhss.sjp.ac.lk>
2025-09-24 10:48:16 +0530

    Import ExperienceSection component into About page and ensure the about page layout rendering

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,14 @@
 import AboutHero from "../components/AboutHero";
 import { SkillsSection } from "../components/SkillsSection";
+import { ExperienceSection } from "../components/ExperienceSection";
 
 export default function About() {
   return (
     <main className="flex flex-col">
       <AboutHero />
       <SkillsSection />
-      {/* Contents for ExperienceSection, EducationSection */}
+      <ExperienceSection />
+      {/* Content for EducationSection */}
     </main>
   );
 }
