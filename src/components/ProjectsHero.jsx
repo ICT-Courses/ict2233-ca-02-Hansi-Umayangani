b21aca204c97cd4a6ddf0074a5ef8dac09e30a2a
Hansi-Umayangani <ar112502@fhss.sjp.ac.lk>
2025-09-24 17:18:46 +0530

    Add ProjectsHero component skeleton with section structure

---
+++ src/components/ProjectsHero.jsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+export function ProjectsHero() {
+  return (
+    <section>
+      <div>
+        <div>
+          {/* Hero Content */}
+        </div>
+      </div>
+    </section>
+  );
+}
