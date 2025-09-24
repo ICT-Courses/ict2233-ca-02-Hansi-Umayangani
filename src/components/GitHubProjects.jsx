f210778183b5cad0f33c4586fdb99fd0e9ebcc7b
Hansi-Umayangani <ar112502@fhss.sjp.ac.lk>
2025-09-24 17:48:09 +0530

    Create GitHubProjects component with section layout

---
+++ src/components/GitHubProjects.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+export function GitHubProjects() {
+  return (
+    <section className="py-20 bg-gradient-to-t from-secondary/5 via-background to-primary/5">
+      <div className="container mx-auto px-4">
+        {/* Content */}
+      </div>
+    </section>
+  );
+}
